e47854bc3d85630affa4158a901a38cb1050d49f
James C <you@example.com>
2022-06-16 13:47:00 -0700

    docs: add caps to png file extension

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 let img = document.createElement('img');
 document.getElementById("answers").appendChild(img);
 
-img.src = '../IMG/magic8ball_start.png';
+img.src = '../IMG/magic8ball_start.PNG';
 
 function shake(){
     const img = document.querySelector('img');
-    img.src = '../IMG/magic8ball_extra.png';
+    img.src = '../IMG/magic8ball_extra.PNG';
     const rando = Math.floor(Math.random() * 20);
-    img.src = '../IMG/magic8ball_' + (rando + 1) + '.png';
+    img.src = '../IMG/magic8ball_' + (rando + 1) + '.PNG';
 }
 
 function displayQuestion(){
