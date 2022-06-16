5d90f6423ea52663385d0e1f33cf9313660c9d92
James C <you@example.com>
2022-06-16 13:39:18 -0700

    feat: add reset field, display questions js, sizing, positioning, responsive width

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,30 @@
-let img = document.querySelector('img');
-img.width = 400;
+let img = document.createElement('img');
+document.getElementById("answers").appendChild(img);
+
 img.src = '../IMG/magic8ball_start.png';
 
 function shake(){
     const img = document.querySelector('img');
     img.src = '../IMG/magic8ball_extra.png';
-    img.width = 400;
     const rando = Math.floor(Math.random() * 20);
-    console.log(rando);
     img.src = '../IMG/magic8ball_' + (rando + 1) + '.png';
 }
 
+function displayQuestion(){
+    const question = document.getElementById('question').value;
+    console.log(question);
+    const displayBox = document.getElementById('displayBox');
+    displayBox.innerHTML = question;
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    shake();
+    displayQuestion();
+    form.reset();
+});
+form.addEventListener("reset", function(event){
+    event.preventDefault();
+   location.reload(true); 
+});
